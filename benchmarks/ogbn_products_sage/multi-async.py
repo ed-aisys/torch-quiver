5d9228797b4b0152a0eaa2381d76934c0ed9ea8f
ZenoTan <zenotan1998@gmail.com>
2021-04-12 07:41:14 +0000

    Dist test

--- benchmarks/ogbn_products_sage/multi-async.py
+++ benchmarks/ogbn_products_sage/multi-async.py
@@ -18,11 +18,22 @@ from torch_geometric.data import NeighborSampler
 import horovod.torch as hvd
 
 p = argparse.ArgumentParser(description='')
-p.add_argument('--node', type=int, default=0)
-p.add_argument('--num_nodes', type=int, default=1)
+# p.add_argument('--node', type=int, default=0)
+# p.add_argument('--num_nodes', type=int, default=1)
 p.add_argument('--gpu', type=int, default=4)
+p.add_argument('--batch_size', type=int, default=512)
 p.add_argument('--ip', type=str, default='localhost')
 args = p.parse_args()
+
+
+class ProductsDataset:
+    def __init__(self, train_idx, edge_index, x, y, f, c):
+        self.train_idx = train_idx
+        self.edge_index = edge_index
+        self.x = x
+        self.y = y
+        self.num_features = f
+        self.num_classes = c
 
 
 class SyncConfig:
@@ -113,38 +124,38 @@ class SAGE(torch.nn.Module):
         return x.log_softmax(dim=-1)
 
 
-def FeatureProcess(sync, x, y, f_rank, f_size):
-    global selector
-    torch.set_num_threads(5)
+# def FeatureProcess(sync, x, y, f_rank, f_size):
+#     global selector
+#     torch.set_num_threads(5)
 
-    import quiver.dist_cpu_sampler as dist
-    comm = dist.Comm(f_rank, f_size)
+#     import quiver.dist_cpu_sampler as dist
+#     comm = dist.Comm(f_rank, f_size)
 
-    def node2rank(nodes):
-        ranks = torch.fmod(nodes, f_size)
-        return ranks
+#     def node2rank(nodes):
+#         ranks = torch.fmod(nodes, f_size)
+#         return ranks
 
-    def local2global(nodes):
-        return nodes
+#     def local2global(nodes):
+#         return nodes
 
-    def global2local(nodes):
-        return nodes
+#     def global2local(nodes):
+#         return nodes
 
-    selector = dist.SyncDistFeatureSelector(
-        comm, ((x, y), local2global, global2local, node2rank))
-    dist.node_features = node_f
-    dist.select_features = select_f
-    port = 27777
-    os.environ['MASTER_ADDR'] = sync.ip
-    os.environ['MASTER_PORT'] = str(port)
-    rpc.init_rpc(f"feature{f_rank}",
-                 rank=sync.rank,
-                 world_size=sync.world_size,
-                 rpc_backend_options=rpc.TensorPipeRpcBackendOptions(
-                     num_worker_threads=1, rpc_timeout=20))
-    sync.barriers[0].wait()
-    rpc.shutdown()
-    time.sleep(30)
+#     selector = dist.SyncDistFeatureSelector(
+#         comm, ((x, y), local2global, global2local, node2rank))
+#     dist.node_features = node_f
+#     dist.select_features = select_f
+#     port = 27777
+#     os.environ['MASTER_ADDR'] = sync.ip
+#     os.environ['MASTER_PORT'] = str(port)
+#     rpc.init_rpc(f"feature{f_rank}",
+#                  rank=sync.rank,
+#                  world_size=sync.world_size,
+#                  rpc_backend_options=rpc.TensorPipeRpcBackendOptions(
+#                      num_worker_threads=1, rpc_timeout=20))
+#     sync.barriers[0].wait()
+#     rpc.shutdown()
+#     time.sleep(30)
 
 
 def SampleProcess(sync, dev, edge_index, train_idx, sizes, batch_size, s_rank,
@@ -235,7 +246,7 @@ def SampleProcess(sync, dev, edge_index, train_idx, sizes, batch_size, s_rank,
     time.sleep(30)
 
 
-def SampleProxy(sync, p_rank, p_size):
+def SampleProxy(sync, p_rank=0, p_size=1):
     os.environ['MASTER_ADDR'] = sync.ip
     port = 27777
     os.environ['MASTER_PORT'] = str(port)
@@ -251,21 +262,19 @@ def SampleProxy(sync, p_rank, p_size):
     time.sleep(30)
 
 
-def TrainProcess(ip, num_batch, g_ranks, c_rank, g_size, c_size, f_rank,
-                 f_size, p_rank, p_size):
+def TrainProcess(ip, num_batch, g_size, batch_size):
+    torch.set_num_threads(5)
     import quiver.dist_cpu_sampler as dist
     hvd.init()
     rank = hvd.rank()
-    ws = hvd.size() + g_size + c_size + f_size + p_size
+    ws = hvd.size() + g_size + 1
     tmp_rank = hvd.size()
     device = torch.device(hvd.local_rank())
-    home = os.getenv('HOME')
-    data_dir = osp.join(home, '.pyg')
-    root = osp.join(data_dir, 'data', 'products')
-    dataset = PygNodePropPredDataset('ogbn-products', root)
-    split_idx = dataset.get_idx_split()
-    train_idx = split_idx['train']
-    data = dataset[0]
+    root = './products.pt'
+    dataset = torch.load(root)
+    train_idx = dataset.train_idx
+    edge_index = dataset.edge_index
+    x, y = dataset.x.share_memory_(), dataset.y.squeeze().share_memory_()
     model = SAGE(dataset.num_features, 256, dataset.num_classes, 3)
     model = model.to(device)
     model.reset_parameters()
@@ -275,44 +284,42 @@ def TrainProcess(ip, num_batch, g_ranks, c_rank, g_size, c_size, f_rank,
     hvd.broadcast_optimizer_state(optimizer, root_rank=0)
     if hvd.local_rank() == 0:
         queues = [mp.Queue(64)]
-        barrier_num = len(g_ranks) + 3
-        if c_rank >= 0:
-            barrier_num += 1
+        barrier_num = g_size + 2
         barriers = [mp.Barrier(barrier_num)]
         samplers = []
         tmp_dev = num_device - 1
-        for g_rank in g_ranks:
+        for i in range(g_size):
             sync = SyncConfig(ip, 0, tmp_rank, ws, queues, barriers, 3, True,
                               True)
             sampler = mp.Process(target=SampleProcess,
-                                 args=(sync, (tmp_dev, 0), data.edge_index,
+                                 args=(sync, (tmp_dev, 0), edge_index,
                                        train_idx, [15, 10, 5],
-                                       512 // hvd.size(), g_rank, g_size))
+                                       batch_size // hvd.size(), i, g_size))
             sampler.start()
             samplers.append(sampler)
             tmp_rank += 1
             tmp_dev -= 1
-        if c_rank >= 0:
-            sync = SyncConfig(ip, 0, tmp_rank, ws, queues, barriers, 3, True,
-                              True)
-            sampler = mp.Process(target=SampleProcess,
-                                 args=(sync, ('cpu', 5), data.edge_index,
-                                       train_idx, [15, 10, 5],
-                                       512 // hvd.size(), c_rank, c_size))
-            sampler.start()
-            samplers.append(sampler)
-            tmp_rank += 1
+        # if c_rank >= 0:
+        #     sync = SyncConfig(ip, 0, tmp_rank, ws, queues, barriers, 3, True,
+        #                       True)
+        #     sampler = mp.Process(target=SampleProcess,
+        #                          args=(sync, ('cpu', 5), data.edge_index,
+        #                                train_idx, [15, 10, 5],
+        #                                512 // hvd.size(), c_rank, c_size))
+        #     sampler.start()
+        #     samplers.append(sampler)
+        #     tmp_rank += 1
         sync = SyncConfig(ip, 0, tmp_rank, ws, queues, barriers, 3, True, True)
         sampler_proxy = mp.Process(target=SampleProxy,
-                                   args=(sync, p_rank, p_size))
+                                   args=(sync,))
         sampler_proxy.start()
         tmp_rank += 1
-        sync = SyncConfig(ip, 0, tmp_rank, ws, queues, barriers, 3, True, True)
-        feature_selector = mp.Process(target=FeatureProcess,
-                                      args=(sync, data.x, data.y.squeeze(),
-                                            f_rank, f_size))
-        feature_selector.start()
-        tmp_rank += 1
+        # sync = SyncConfig(ip, 0, tmp_rank, ws, queues, barriers, 3, True, True)
+        # feature_selector = mp.Process(target=FeatureProcess,
+        #                               args=(sync, data.x, data.y.squeeze(),
+        #                                     f_rank, f_size))
+        # feature_selector.start()
+        # tmp_rank += 1
     port = 27777
     os.environ['MASTER_ADDR'] = ip
     os.environ['MASTER_PORT'] = str(port)
@@ -333,38 +340,40 @@ def TrainProcess(ip, num_batch, g_ranks, c_rank, g_size, c_size, f_rank,
     for i in range(num_batch):
         beg = time.time()
         sample = rpc.rpc_sync(
-            f"proxy{p_rank}",  # TODO: distributed
+            f"proxy0",  # TODO: distributed
             get_sample,
-            args=(p_rank, ),
+            args=(0, ),
             kwargs=None,
             timeout=-1.0)
         _, sample = sample
         batch_size, n_id, adjs = sample
         sample_time += time.time() - beg
         beg = time.time()
-        feature = rpc.rpc_sync(
-            f"feature{f_rank}",  # TODO: distributed
-            select_features,
-            args=(n_id, True),
-            kwargs=None,
-            timeout=-1.0)
-        label = rpc.rpc_sync(
-            f"feature{f_rank}",  # TODO: distributed
-            select_features,
-            args=(n_id[:batch_size], False),
-            kwargs=None,
-            timeout=-1.0)
+        feature = x[n_id].to(device)
+        label = y[n_id[:batch_size]].to(device)
+        # feature = rpc.rpc_sync(
+        #     f"feature{f_rank}",  # TODO: distributed
+        #     select_features,
+        #     args=(n_id, True),
+        #     kwargs=None,
+        #     timeout=-1.0)
+        # label = rpc.rpc_sync(
+        #     f"feature{f_rank}",  # TODO: distributed
+        #     select_features,
+        #     args=(n_id[:batch_size], False),
+        #     kwargs=None,
+        #     timeout=-1.0)
         feature_time += time.time() - beg
         beg = time.time()
         adjs = [adj.to(device) for adj in adjs]
-        feature, order0 = feature
-        label, order1 = label
-        feature = torch.cat([f.to(device) for f in feature])
-        label = torch.cat([l.to(device) for l in label])
-        origin_feature = torch.empty_like(feature)
-        origin_label = torch.empty_like(label)
-        origin_feature[order0] = feature
-        origin_label[order1] = label
+        # feature, order0 = feature
+        # label, order1 = label
+        # feature = torch.cat([f.to(device) for f in feature])
+        # label = torch.cat([l.to(device) for l in label])
+        # origin_feature = torch.empty_like(feature)
+        # origin_label = torch.empty_like(label)
+        # origin_feature[order0] = feature
+        # origin_label[order1] = label
 
         optimizer.zero_grad()
         out = model(feature, adjs)
@@ -374,19 +383,17 @@ def TrainProcess(ip, num_batch, g_ranks, c_rank, g_size, c_size, f_rank,
         train_time += time.time() - beg
     hvd.allreduce(torch.tensor(0), name='barrier')
     t = time.time() - t0
-    print(f'took {t}')
-    print(f'sample {sample_time}')
-    print(f'feature {feature_time}')
-    print(f'train {train_time}')
+    if rank == 0:
+        print(f'took {t}')
+        print(f'sample {sample_time}')
+        print(f'feature {feature_time}')
+        print(f'train {train_time}')
     rpc.shutdown()
 
 
 if __name__ == '__main__':
     mp.set_start_method('spawn')
-    g_beg = args.node * args.gpu
-    g_end = g_beg + args.gpu
-    g_ranks = list(range(g_beg, g_end))
-    g_size = args.gpu * args.num_nodes
+    batch_size = args.batch_size
+    g_size = args.gpu
 
-    TrainProcess(args.ip, 192, g_ranks, -1, g_size, 0, args.node,
-                 args.num_nodes, args.node, args.num_nodes)
+    TrainProcess(args.ip, 192, g_size, batch_size)
